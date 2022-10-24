68398c26c6ff7e199cca8e6542d43a164d8a5716
Adriano Todaro <a.todaro.lcg@gmail.com>
2022-10-24 22:16:48 +0200

    implement codestatsservice

---
+++ src/main/kotlin/com/example/codestats/service/CodeStatsServiceImpl.kt
@@ -0,0 +1,26 @@
+package com.example.codestats.service
+
+import com.example.codestats.model.dto.LanguagePercentages
+import com.example.codestats.repository.GitRepoDataRepository
+import common.model.LanguageId
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Service
+
+@Service
+@Profile("!test")
+class CodeStatsServiceImpl(
+    private val gitRepoDataRepository: GitRepoDataRepository
+) : CodeStatsService {
+    override fun getLanguagePercentages(): LanguagePercentages {
+        val perRepoLanguageBytes = gitRepoDataRepository.getLanguageBytes()
+
+        val totalBytes = perRepoLanguageBytes.flatMap { it.value.values }.sum().toDouble()
+        val languageBytes = mutableMapOf<LanguageId, Long>()
+
+        perRepoLanguageBytes.flatMap { it.value.entries }.forEach {
+            languageBytes[it.key] = languageBytes.getOrDefault(it.key, 0) + it.value
+        }
+
+        return languageBytes.mapValues { it.value / totalBytes }
+    }
+}
---
+++ src/test/kotlin/com/example/codestats/service/CodeStatsServiceImplTest.kt
@@ -0,0 +1,49 @@
+package com.example.codestats.service
+
+import com.example.codestats.repository.GitRepoDataRepository
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class CodeStatsServiceImplTest {
+
+    private val repository: GitRepoDataRepository = mockk()
+    private val service = CodeStatsServiceImpl(repository)
+
+    @BeforeAll
+    fun setUp() {
+        every { repository.getLanguageBytes() } returns mockData
+    }
+
+    @Test
+    fun `Should produce the right Language percentages`() {
+        val expected = mapOf(
+            "language_a" to 0.1,
+            "language_b" to 0.6,
+            "language_c" to 0.3
+        )
+        val actual = service.getLanguagePercentages()
+
+        Assertions.assertEquals(expected, actual)
+    }
+
+    companion object {
+        val mockData = mapOf(
+            1L to mapOf(
+                "language_a" to 10L,
+                "language_b" to 40L,
+            ),
+            2L to mapOf(
+                "language_b" to 20L,
+                "language_c" to 30L
+            )
+        )
+    }
+}
