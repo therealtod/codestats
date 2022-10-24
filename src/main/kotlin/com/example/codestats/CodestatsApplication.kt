ec4b9ab4a75b5719f85caa07503c0f9e86697388
Adriano Todaro <a.todaro.lcg@gmail.com>
2022-10-24 13:49:33 +0200

    initialize projecte project

---
+++ src/main/kotlin/com/example/codestats/CodestatsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.codestats
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodestatsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CodestatsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/codestats/CodestatsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.codestats
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodestatsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
