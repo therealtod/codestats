a0f54b1bea6fa413a730d28e7553dd54de88548c
Adriano Todaro <a.todaro.lcg@gmail.com>
2022-10-26 18:17:08 +0200

    schedule fetching job once per day

--- src/main/kotlin/com/example/codestats/scheduledtasks/GithubDataFetchTasks.kt
+++ src/main/kotlin/com/example/codestats/scheduledtasks/GithubDataFetchTasks.kt
@@ -1,17 +1,9 @@
 package com.example.codestats.scheduledtasks
 
-import com.example.codestats.LanguageFactory
-import com.example.codestats.LanguageUsageBytesFactory
-import com.example.codestats.RepositoryDataFactory
-import com.example.codestats.client.GithubClient
-import com.example.codestats.model.tableRow.RepositoryData
-import com.example.codestats.repository.GitHubRepoRepository
-import com.example.codestats.repository.LanguageRepository
 import com.example.codestats.service.GithubDataService
 import com.example.codestats.service.PersistenceService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.net.URI
 
 @Component
 class GithubDataFetchTasks(
@@ -19,9 +11,9 @@ class GithubDataFetchTasks(
     private val githubDataService: GithubDataService
 ) {
 
-    @Scheduled(fixedDelay = 20000)
+    @Scheduled(cron = "0 0 0 * * ?")
     fun fetchProductboardGithubData() {
-
+        persistenceService.clearTables()
         val data = githubDataService.getLanguageUsageData("productboard")
         persistenceService.batchWriteRepositoryData(data)
     }
