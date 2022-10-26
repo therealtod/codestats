50c9c127a9c24ff029e06a329ebf96612b94f0fd
Adriano Todaro <a.todaro.lcg@gmail.com>
2022-10-26 22:40:35 +0200

    allow controlling percentages precision

--- src/main/kotlin/com/example/codestats/service/implementation/CodeStatsServiceImpl.kt
+++ src/main/kotlin/com/example/codestats/service/implementation/CodeStatsServiceImpl.kt
@@ -1,16 +1,22 @@
 package com.example.codestats.service.implementation
 
+import com.example.codestats.extensions.round
 import com.example.codestats.model.dto.LanguagePercentages
 import com.example.codestats.repository.LanguageRepository
 import com.example.codestats.service.CodeStatsService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class CodeStatsServiceImpl(
-    private val languageRepository: LanguageRepository
+    private val languageRepository: LanguageRepository,
+    @Value("\${codestats.language.percentages.precision.decimals}")
+    private val allowedDecimals: Int
 ) : CodeStatsService {
     override fun getLanguagePercentages(): LanguagePercentages {
         val dbPercentages = languageRepository.getLanguageUsagePercentages()
-        return dbPercentages.associate{ Pair(it.languageName, it.percentage) }.toMap()
+        return dbPercentages
+            .filter { it.percentage >= 1.0/(10*allowedDecimals) }
+            .associate{ Pair(it.languageName, it.percentage.round(allowedDecimals)) }.toMap()
     }
 }
