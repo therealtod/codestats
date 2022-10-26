ca9469fd9d4ebff282202a1128450760cb6581aa
Adriano Todaro <a.todaro.lcg@gmail.com>
2022-10-26 13:43:38 +0200

    wrap repositories in persistence service layer

---
+++ src/main/kotlin/com/example/codestats/service/PersistenceService.kt
@@ -0,0 +1,7 @@
+package com.example.codestats.service
+
+import com.example.codestats.model.bom.GithubRepository
+
+interface PersistenceService {
+    fun batchWriteRepositoryData(data: Collection<GithubRepository>)
+}
