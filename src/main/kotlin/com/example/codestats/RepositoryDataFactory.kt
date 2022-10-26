b4d8a56653c5e4baf11b108bc42283c425b904c8
Adriano Todaro <a.todaro.lcg@gmail.com>
2022-10-26 13:39:06 +0200

    modify repositorydata model

--- src/main/kotlin/com/example/codestats/RepositoryDataFactory.kt
+++ src/main/kotlin/com/example/codestats/RepositoryDataFactory.kt
@@ -1,10 +1,10 @@
 package com.example.codestats
 
-import com.example.codestats.model.dto.GithubRepoDetailsReply
+import com.example.codestats.model.bom.GithubRepository
 import com.example.codestats.model.tableRow.RepositoryData
 
 object RepositoryDataFactory {
-    fun createRepositoryData(repoDetails: GithubRepoDetailsReply): RepositoryData{
-        return RepositoryData()
+    fun createRepositoryData(repo: GithubRepository): RepositoryData{
+        return RepositoryData(gitHubId = repo.gitHubId)
     }
 }
--- src/main/kotlin/com/example/codestats/model/tableRow/RepositoryData.kt
+++ src/main/kotlin/com/example/codestats/model/tableRow/RepositoryData.kt
@@ -1,5 +1,6 @@
 package com.example.codestats.model.tableRow
 
+import com.example.codestats.model.RepositoryId
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.PersistenceCreator
 import org.springframework.data.relational.core.mapping.Column
@@ -9,8 +10,10 @@ import org.springframework.data.relational.core.mapping.Table
 data class RepositoryData(
     @Id
     @Column("repository_id")
-    val repositoryId: Long
+    val repositoryId: RepositoryId,
+    @Column("github_id")
+    val gitHubId: Long
 ){
     @PersistenceCreator
-    constructor() : this(0)
+    constructor(gitHubId: Long) : this(0, gitHubId)
 }
