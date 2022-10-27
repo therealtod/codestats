e4c8e77ec6b76069b77b651fcd1a3d9ad1c840a6
Adriano Todaro <a.todaro.lcg@gmail.com>
2022-10-27 15:33:12 +0200

    avoid using real client in tests

--- src/test/kotlin/com/example/codestats/AbstractSpringBootIntegrationTest.kt
+++ src/test/kotlin/com/example/codestats/AbstractSpringBootIntegrationTest.kt
@@ -1,8 +1,10 @@
 package com.example.codestats
 
+import com.example.codestats.client.GithubClient
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 
@@ -15,4 +17,7 @@ abstract class AbstractSpringBootIntegrationTest {
 
     @Autowired
     protected lateinit var mockMvc: MockMvc
+
+    @MockBean
+    protected lateinit var githubClient: GithubClient
 }
