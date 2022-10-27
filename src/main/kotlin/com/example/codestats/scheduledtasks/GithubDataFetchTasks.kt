24fe9bdc3b2b5b215d9493fb093a8aa24c4a12c2
Adriano Todaro <a.todaro.lcg@gmail.com>
2022-10-27 14:55:29 +0200

    add logging to task

--- src/main/kotlin/com/example/codestats/scheduledtasks/GithubDataFetchTasks.kt
+++ src/main/kotlin/com/example/codestats/scheduledtasks/GithubDataFetchTasks.kt
@@ -2,8 +2,10 @@ package com.example.codestats.scheduledtasks
 
 import com.example.codestats.service.GithubDataService
 import com.example.codestats.service.PersistenceService
+import org.apache.logging.log4j.LogManager
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
 @Component
 class GithubDataFetchTasks(
@@ -11,10 +13,21 @@ class GithubDataFetchTasks(
     private val githubDataService: GithubDataService
 ) {
 
+    @PostConstruct
     @Scheduled(cron = "0 0 0 * * ?")
     fun fetchProductboardGithubData() {
+        log.info("clearing DB tables...")
         persistenceService.clearTables()
+        log.info("DONE")
+        log.info("Fetching data from github...")
         val data = githubDataService.getLanguageUsageData("productboard")
+        log.info("DONE")
+        log.info("Writing records to DB")
         persistenceService.batchWriteRepositoryData(data)
+        log.info("DONE")
+    }
+
+    companion object {
+        private val log = LogManager.getLogger(GithubDataFetchTasks::class.java)
     }
 }
