8c8d612b3c294c5773a6cacd054eb7c13161c927
Adriano Todaro <a.todaro.lcg@gmail.com>
2022-10-27 15:00:59 +0200

    parametrize org id

--- src/main/kotlin/com/example/codestats/scheduledtasks/GithubDataFetchTasks.kt
+++ src/main/kotlin/com/example/codestats/scheduledtasks/GithubDataFetchTasks.kt
@@ -3,6 +3,7 @@ package com.example.codestats.scheduledtasks
 import com.example.codestats.service.GithubDataService
 import com.example.codestats.service.PersistenceService
 import org.apache.logging.log4j.LogManager
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
@@ -10,17 +11,19 @@ import javax.annotation.PostConstruct
 @Component
 class GithubDataFetchTasks(
     private val persistenceService: PersistenceService,
-    private val githubDataService: GithubDataService
+    private val githubDataService: GithubDataService,
+    @Value("\${codestats.target.orgid}")
+    private val orgId: String?
 ) {
-
     @PostConstruct
     @Scheduled(cron = "0 0 0 * * ?")
     fun fetchProductboardGithubData() {
+        val targetOrgId = orgId ?: DEFAULT_ORG_ID
         log.info("clearing DB tables...")
         persistenceService.clearTables()
         log.info("DONE")
         log.info("Fetching data from github...")
-        val data = githubDataService.getLanguageUsageData("productboard")
+        val data = githubDataService.getLanguageUsageData(targetOrgId)
         log.info("DONE")
         log.info("Writing records to DB")
         persistenceService.batchWriteRepositoryData(data)
@@ -29,5 +32,6 @@ class GithubDataFetchTasks(
 
     companion object {
         private val log = LogManager.getLogger(GithubDataFetchTasks::class.java)
+        private const val DEFAULT_ORG_ID = "productboard"
     }
 }
